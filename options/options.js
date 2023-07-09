f5e5233ae341182ac061220b4f46e890c56a58c9
Crowdbotics <team@crowdbotics.com>
2023-07-09 03:24:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steponepoker-42016",
+  url: "https://steponepoker-42016.botics.co",
+  api: "https://steponepoker-42016.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
